f69caf96dd977543d8e0e7f44f7e55012754d56e
Abisha <104752618+abisha2808@users.noreply.github.com>
2026-02-06 17:48:14 +0530

    Update UserController.java

--- src/main/java/abisha/com/com/controller/UserController.java
+++ src/main/java/abisha/com/com/controller/UserController.java
@@ -21,8 +21,8 @@ import abisha.com.com.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:5173",
-"https://money-management-frontend.vercel.app")
+@CrossOrigin(origins = {"http://localhost:5173",
+"https://money-management-frontend.vercel.app"})
 public class UserController {
 	
 	@Autowired
